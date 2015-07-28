c6dfc94a03d0fd2fbd7724ca407bc6697ce26b53
zachlandes <zlandes@Zacharys-MacBook-Pro.local>
2015-07-28 13:42:35 -0700

    PyMC Disaster Model

---
+++ disaster_model.py
@@ -0,0 +1,31 @@
+from pymc import DiscreteUniform, Exponential, deterministic, Poisson, Uniform
+import numpy as np
+
+disasters_array = \
+        np.array([ 4, 5, 4, 0, 1, 4, 3, 4, 0, 6, 3, 3, 4, 0, 2, 6,
+                   3, 3, 5, 4, 5, 3, 1, 4, 4, 1, 5, 5, 3, 4, 2, 5,
+                   2, 2, 3, 4, 2, 1, 3, 2, 2, 1, 1, 1, 1, 3, 0, 0,
+                   1, 0, 1, 1, 0, 0, 3, 1, 0, 3, 2, 2, 0, 1, 1, 1,
+                   0, 1, 0, 1, 0, 0, 0, 2, 1, 0, 0, 0, 1, 1, 0, 2,
+                   3, 3, 1, 1, 2, 1, 1, 1, 1, 2, 4, 2, 0, 0, 1, 4,
+                   0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1])
+
+# switchpoint, all values equally likely a priori
+switchpoint = DiscreteUniform('switchpoint', lower=0, upper=110, doc='Switchpoint[year]')
+
+early_mean = Exponential('early_mean', beta=1.)
+late_mean = Exponential('late_mean', beta=1.)
+
+"""In PyMC terminology, the parameter rate is 'deterministic', meaning it's determined explicitly
+by observations of its parents. (It's still a random variable)"""
+@deterministic(plot=False)
+def rate(s=switchpoint, e=early_mean, l=late_mean):
+    # concatenate poisson means
+    out = np.empty(len(disasters_array)) # array of size 1 x len(disasters_array) with uninitialized values
+    out[:s] = e
+    out[s:] = l
+    return out                           # looks like [e, e, ..., e, l, l,...,l]
+
+"""Finally, we model the observed random variable, disasters. We have to assign it a probability distribution
+because we want to use Bayes' Theorem"""
+disasters = Poisson('disasters', mu=rate, value=disasters_array, observed=True) # note the declaration of observed
